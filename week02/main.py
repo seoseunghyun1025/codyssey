d808fe3493098bddb3c254c60aa1281d9e403fc5
SeoSeungHyun <seunghyun.seo101@gmail.com>
2025-03-20 12:39:43 +0900

    feat: 0.7이상인 데이터 csv 포맷 저장 기능 구현

--- week02/main.py
+++ week02/main.py
@@ -1,12 +1,13 @@
 # main.py
 # 2025-03-19
 
-FILE_NAME = 'week02/Mars_Base_Inventory_List.csv'
+INPUT_FILE_NAME = 'week02/Mars_Base_Inventory_List.csv'
+OUTPUT_FILE_NAME = 'week02/Mars_Base_Inventory_danger.csv'
 
 
 def get_data_from_csv():
     try:
-        with open(FILE_NAME, 'r', encoding='utf-8') as file:
+        with open(INPUT_FILE_NAME, 'r', encoding='utf-8') as file:
             data = file.read().splitlines()
             return [line.split(',') for line in data]
     except Exception as e:
@@ -33,10 +34,8 @@ def get_flammability_values(data):
     return []
 
 
-# "Flammability" 값이 0.7 이상인 항목만 필터링하여 반환하는 함수
 def filter_flammability_above_0_7(sorted_inventory):
     try:
-        # 'sorted_inventory'에서 0.7 이상인 값만 필터링
         filtered_inventory = [
             row for row in sorted_inventory if float(row[4]) >= 0.7
         ]
@@ -47,9 +46,29 @@ def filter_flammability_above_0_7(sorted_inventory):
     return []
 
 
+def save_to_csv(data):
+    try:
+        with open(OUTPUT_FILE_NAME, 'w', encoding='utf-8') as file:
+            header = [
+                "Substance",
+                "Weight (g/cm³)",
+                "Specific Gravity",
+                "Strength",
+                "Flammability",
+            ]
+            file.write(','.join(header) + '\n')
+            for row in data:
+                file.write(','.join(row) + '\n')
+        print(f"Data successfully saved to {OUTPUT_FILE_NAME}")
+    except Exception as e:
+        print(f"Error saving to CSV: {e}")
+    return []
+
+
 if __name__ == '__main__':
     inventory_list = get_data_from_csv()
     print(inventory_list)
     sorted_inventory = get_flammability_values(inventory_list)
     filtered_inventory = filter_flammability_above_0_7(sorted_inventory)
     print(filtered_inventory)
+    save_to_csv(filtered_inventory)
