8145116633405370af18267309dd9d367096c7ef
SeoSeungHyun <seunghyun.seo101@gmail.com>
2025-03-20 12:28:24 +0900

    feat: 인화성이 0.7 이상인 리스트만 출력 기능 구현

--- week02/main.py
+++ week02/main.py
@@ -33,7 +33,23 @@ def get_flammability_values(data):
     return []
 
 
+# "Flammability" 값이 0.7 이상인 항목만 필터링하여 반환하는 함수
+def filter_flammability_above_0_7(sorted_inventory):
+    try:
+        # 'sorted_inventory'에서 0.7 이상인 값만 필터링
+        filtered_inventory = [
+            row for row in sorted_inventory if float(row[4]) >= 0.7
+        ]
+
+        return filtered_inventory
+    except Exception as e:
+        print(f"Error: {e}")
+    return []
+
+
 if __name__ == '__main__':
     inventory_list = get_data_from_csv()
     print(inventory_list)
     sorted_inventory = get_flammability_values(inventory_list)
+    filtered_inventory = filter_flammability_above_0_7(sorted_inventory)
+    print(filtered_inventory)
