8e58034e0fb62d6d9e0d43ebb4450f8828121615
SeoSeungHyun <seunghyun.seo101@gmail.com>
2025-03-26 16:56:36 +0900

    feat: 사전 객체 추가

--- week03/main.py
+++ week03/main.py
@@ -1,14 +1,14 @@
 class DummySensor:
 
-    def __init__(self, name="DefaultSensor"):
+    def __init__(self, name='DefaultSensor'):
         self.name = name
-        print(f"{self.name}가 생성되었습니다.")
+        print(f'{self.name}가 생성되었습니다.')
 
         self.env_values = {
-            "mars_base_internal_temperature": None,
-            "mars_base_external_temperature": None,
-            "mars_base_internal_humidity": None,
-            "mars_base_external_illuminance": None,
-            "mars_base_internal_co2": None,
-            "mars_base_internal_oxygen": None,
+            'mars_base_internal_temperature': None,
+            'mars_base_external_temperature': None,
+            'mars_base_internal_humidity': None,
+            'mars_base_external_illuminance': None,
+            'mars_base_internal_co2': None,
+            'mars_base_internal_oxygen': None,
         }
