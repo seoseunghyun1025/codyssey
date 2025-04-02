e2d2480311d66ac33a430d188c6ce5705496095c
SeoSeungHyun <seunghyun.seo101@gmail.com>
2025-04-02 14:35:09 +0900

    feat: env_value 속성 dict 객체 구현

--- week04/mars_mission_computer.py
+++ week04/mars_mission_computer.py
@@ -1,2 +1,10 @@
 class MissionComputer:
-    pass
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': None,
+            'mars_base_external_temperature': None,
+            'mars_base_internal_humidity': None,
+            'mars_base_external_illuminance': None,
+            'mars_base_internal_co2': None,
+            'mars_base_internal_oxygen': None
+        }
